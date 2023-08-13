b7854726d491931c4fea197d71a63d4c4ab00221
Craig Davidson <davidson-c@outlook.com>
2023-08-13 21:35:46 +0100

    Alt note display

--- src/pages/Performance.js
+++ src/pages/Performance.js
@@ -1,8 +1,30 @@
+import { useEffect } from "react"
+import staff from "../images/comp2.svg"
+import note from "../images/note.svg"
+import note2 from "../images/note2.svg"
+import { useState } from "react"
+
+
 export default function Performance() {
-  
+  const [noteImage,setNoteImage] = useState(note2)
+
+  useEffect(() => {
+    setNoteImage(note2)
+  })  
     return (
-      <div className="Performance">
+      <div className="performance">
         <h1>Performance</h1>
+        <div className="testContainer">
+          <div className="testSpacer"></div>
+          <div className="testImageContainer">
+           <img src={staff} alt="GrandStaff" className="grandStaff"></img>
+           <div className="noteContainer">
+            <img src={noteImage} alt="GrandStaff" className="grandStaff"></img>
+          </div>
+          </div>
+          <div className="testSpacer"></div>
+          
+        </div>
       </div>
     )
 }
