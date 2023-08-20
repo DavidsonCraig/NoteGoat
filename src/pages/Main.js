11a9c58c92333203ca9b850a06c1b0bbb198837b
Craig Davidson <davidson-c@outlook.com>
2023-08-20 19:12:52 +0100

    Finished Menu Piano decoration

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,31 @@
 import { NavLink } from "react-router-dom";
+import piano from "../images/piano.png"
+import { useEffect } from "react";
+import anime from "animejs";
 
 export default function Main(props) {
 
+    useEffect(() => {
+      console.log("Bruh")
+      anime({
+        targets: '.pianoMenuDecoration',
+        translateY: ['25vh', '0vh'],
+        duration: 4000,
+        loop: true,
+        easing: 'linear',
+      });
+    },[])
+
     return (
       <div className="main">
-        <h1>Main</h1>
-        <NavLink to="/sightread">Learn to read</NavLink>
-        <br></br>
-        <NavLink to="/performance">Learn to play</NavLink>
-        <br></br>
-        <NavLink to="/settings">Settings</NavLink>
+        <div className="pianoMenuDecorationContainer">
+          <img src={piano} className="pianoMenuDecoration"></img>
+        </div>
+        <div className="menuOptionContainer">
+          <NavLink to="/sightread" className="menuOption">Learn to read</NavLink>
+          <NavLink to="/performance" className="menuOption">Learn to play</NavLink>
+          <NavLink to="/settings" className="menuOption">Settings</NavLink>
+        </div>
       </div>
     )
 }
