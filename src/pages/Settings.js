4dd02d6018aebecd2113469dffe364b4e03421f1
Craig Davidson <davidson-c@outlook.com>
2023-08-24 16:55:28 +0100

    Added goat

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,8 +1,12 @@
+import goat from "../images/goat640.png"
+
+
 export default function Settings() {
   
     return (
       <div className="settings">
         <h1>Settings</h1>
+        <img src={goat}></img>
       </div>
     )
 }
